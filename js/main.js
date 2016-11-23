7625b0b7a45d2d20105157ebd411e94eac2c9e48
Tyler <tcameronsmith@gmail.com>
2016-11-23 17:58:26 -0500

    add error class to submit function when clicked and there are no inputs

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
-$('.signin').click(function() {
-	$('.modal').fadeIn('start');
-});
 
-$('.close').click(function() {
-	$('.modal').fadeOut('start');
+$(document).ready(function() {
+
+
+		$('.signin').click(function() {
+			$('.modal').fadeIn('start');
+		});
+
+		$('.close').click(function() {
+			$('.modal').fadeOut('start');
+		});
+
+		$('.submit').click(function() {
+			if ($("form input[name='user']").val().length === 0) {
+				$("input[name='user']").addClass('error');
+			} else if ($("form input[name='pass']").val().length === 0) {
+				$("form input[name='pass']").addClass('error');
+			} else {
+				$('.modal').fadeOut('start');
+			}
+		});
 });
