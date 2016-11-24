eafa0b2999b0c82cc7d5e1b1d86218638c4d0c9a
Tyler <tcameronsmith@gmail.com>
2016-11-23 21:10:43 -0500

    add event.stop propagation to submit, form input, and form clicks. now have modal fadeout when clicked

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 
 $(document).ready(function() {
 
-
 		$('.signin').click(function() {
 			$('.modal').fadeIn('start');
 		});
@@ -13,15 +12,25 @@ $(document).ready(function() {
 		$('.submit').click(function() {
 			if ($("form input[name='user']").val().length === 0) {
 				$("input[name='user']").addClass('error');
+					event.stopPropagation();
 			} else if ($("form input[name='pass']").val().length === 0) {
 				$("form input[name='pass']").addClass('error');
+					event.stopPropagation();
 			} else {
 				$('.modal').fadeOut('start');
 			}
 		});
 
-		$('input').click(function() {
+		$('form input').click(function() {
 			$("input").removeClass('error');
+			event.stopPropagation();
+		});
 
+		$('form').click(function() {
+			event.stopPropagation();
+		});
+
+		$('.modal').click(function() {
+			$('.modal').fadeOut('start');
 		});
 });
